36d6dc37c1257d57799d97ed5ce0528d06cfe10c
Dong <gongan1556@naver.com>
2022-10-26 22:27:21 +0900

    jupyter lab 2일차(04,10 과제 및 마무리 )

--- 04adv_jtbcnews.py
+++ 04adv_jtbcnews.py
@@ -7,46 +7,8 @@
 import requests
 from lxml import html
 
-url = 'https://news.jtbc.co.kr/section/list.aspx?{pdate}&scode=&copyright='
-header = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'}
-
-news = [titles,contents,category,writers,regdate]
-titles,contents,category,writers,regdate = [],[],[],[],[]
-
-# for i in range(1,2+1):
-# 질의문자열을 위해 params 변수 정의
-param = {'pgi': i}
-# http header와 질의문자열을 이용해서 requests 모듈 호출
-res = requests.get(url, headers=header, params=param)
-doctree = html.fromstring(res.text)
-
-docs = doctree.cssselect('dt.title_cr a')
-for title in docs:
-    tit = title.text_content()
-    titles.append(tit)
-
-docs = doctree.cssselect('dd.desc a')
-for content in docs:
-    cnt = content.text_content().strip().replace('   ','')
-    contents.append(cnt)
-
-docs = doctree.cssselect('span.location')
-for ctg in docs:
-    cate = ctg.text_content().replace('[JTBC','').replace(']','').replace('>','').strip()
-    category.append(cate)
-
-docs = doctree.cssselect('span.writer')
-for writer in docs:
-    wrt = writer.text_content().strip()
-    writers.append(wrt)
-
-docs = doctree.cssselect('span.date')
-for rgd in docs:
-    pdate = rgd.text_content().strip()
-    regdate.append(to24h(pdate))
-print(news)
-
-
+url = 'https://news.jtbc.co.kr/section/list.aspx'
+headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'}
 def to24h(time):
     # 2022-10-26 오전 11:56:00
     pd = time.split(' ')
@@ -61,9 +23,63 @@ def to24h(time):
     time = f'{pd[0]} {pt[0]}:{pt[1]}:{pt[2]}'
     return time
 
+titles,contents = [],[]
+category,writers,regdate = [],[],[]
+news = [titles,contents,category,writers,regdate]
+
+for i in range(20210715,20210720+1):
+# 질의문자열을 위해 params 변수 정의
+    params = {'pdate': i}
+    res = requests.get(url, headers=headers, params=params)
+    doctree = html.fromstring(res.text)
+
+    docs = doctree.cssselect('dt.title_cr a')
+    for title in docs:
+        tit = title.text_content().replace(',','|')
+        titles.append(tit)
+
+    docs = doctree.cssselect('dd.desc a')
+    for content in docs:
+        cnt = content.text_content().strip().replace('   ','').replace(',','|')
+        contents.append(cnt)
+
+    docs = doctree.cssselect('span.location')
+    for ctg in docs:
+        cate = ctg.text_content().replace('[','').replace(']','').replace('JTBC','').replace('연합','').replace('>','').strip().replace(',','|').replace('\t','').replace('\n','')
+        category.append(cate)
+
+    docs = doctree.cssselect('span.writer')
+    for writer in docs:
+        wrt = writer.text_content().strip().replace(',','|')
+        writers.append(wrt)
+
+    docs = doctree.cssselect('span.date')
+    for rgd in docs:
+        pdate = rgd.text_content().strip().replace(',','|')
+        regdate.append(to24h(pdate))
+print(news)
+
+# csv
 header = '제목,기사미리보기,카테고리,기자,날짜\t\n'
-with open('data/JTBC.csv','a',encoding='utf-8') as f:
+with open('data/JTBC_P.csv','a',encoding='utf-8') as f:
     f.write(header)
     for i in range(len(titles)):
         title, content, ctg, writer, rgd = titles[i],contents[i],category[i], writers[i], regdate[i]
         f.write(f'"{title}","{content}","{ctg}","{writer}",{rgd}\n')
+
+# json
+import json
+from collections import OrderedDict
+
+news = []
+
+with open('data/JTBC_P.json','a',encoding='UTF-8') as f:
+    for i in range(len(titles)):
+        new = OrderedDict()
+        new['제목'] = titles[i]
+        new['기사미리보기'] = contents[i]
+        new['카테고리'] = category[i]
+        new['기자'] = writers[i]
+        new['날짜'] = regdate[i]
+        news.append(new)
+    f.write(json.dumps(news,ensure_ascii=False))
--- 10selenium.py
+++ 10selenium.py
@@ -1,9 +1,9 @@
-# selenium으로 스크레핑 하기
-# 웹브라우저를 이용한 작업들을
-# 자동화할 수 있도록 특수제작된 브라우저
+## selenium으로 스크레핑 하기
+# 웹브라우저를 이용한 작업들을 자동화할 수 있도록 특수제작된 브라우저
 # 또한, ajax를 이용한 동적 웹페이지를 크롤링하는데에도 사용
+# requests, bs4로 스크래핑할 수 없는 동적 데이터를 포함하는 웹 페이지를 원격 조작이 가능한 웹브라우저를 이용해서 처리
 
-# seleniumhq.org
+## seleniumhq.org
 # chromedriver.chromium.org
 # ChromeDriver 98.0.4758.102 (2022-02-17)
 # chromedriver_win32.zip => chromedriver.exe
@@ -11,7 +11,60 @@
 
 # pip install selenium => selenium-4.1.0 (2022-02-17)
 
-# requests, bs4로 스크래핑할 수 없는
-# 동적 데이터를 포함하는 웹 페이지를
-# 원격 조작이 가능한 웹브라우저를 이용해서 처리
+# 한빛미디어에서 전체도서목록 크롤링하기
+# 도서명, 저자, 가격, 발행일 추출
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+url = 'https://www.hanbit.co.kr/store/books/full_book_list.html'
+headers = {'https://product.kyobobook.co.kr/api/gw/pub/pdt/best-seller/online?page=1&per=20&period=001&dsplDvsnCode=000&dsplTrgtDvsnCode=001'}
+
+# selenium 초기화
+chrome = webdriver.Chrome(r'C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
+
+# 지정한 url로 접속
+chrome.get(url)
+
+# 응답받은 웹소스를 bs4에 저장
+html = BeautifulSoup(chrome.page_source,'lxml')
+
+# selenium 종료
+chrome.close()
+
+# 도서명, 저자, 출판일, 가격 추출
+titles,writers,regdate,prices = [],[],[],[]
+books_len = int(len(html.select('td'))/5)
+
+
+for i in range(0,books_len+2,5):
+    titles.append(html.select('td')[i+1].text.replace(',',''))
+    writers.append(html.select('td')[i+2].text)
+    regdate.append(html.select('td')[i+3].text)
+    prices.append(html.select('td')[i+4].text.replace(',','').replace('원',''))
+
+# csv저장
+header = '도서명,저자,출판일,가격\n'
+
+with open('data/allbooks.csv','a',encoding='utf-8') as f:
+    f.write(header)
+    for i in range(len(titles)):
+        title, writer = titles[i], writers[i]
+        rdg, price =  regdate[i], prices[i]
+        f.write(f' "{title}","{writer}","{regdate}","{price}"\n ')
+
+# JSON
+import json
+from collections import OrderedDict
+
+books = []
+
+with open('data/allbook.json','a',encoding='UTF-8') as f:
+    for i in range(len(titles)):
+        book = OrderedDict()
+        book['도서명'] = titles[i]
+        book['저자'] = writers[i]
+        book['출판일'] = regdate[i]
+        book['가격'] = prices[i]
+        books.append(book)
+    f.write(json.dumps(books,ensure_ascii=False))
 
